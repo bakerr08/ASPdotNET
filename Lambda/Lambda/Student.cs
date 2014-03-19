0f667826da5921ea4de357054de9c730a145be7d
bakerr08 <bakerr08@gmail.com>
2014-03-18 19:34:46 -0500

    lambda

---
+++ Lambda/Lambda/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    public class Student
+    {
+        public List<int> ValidSutdentIDs(List<int> IDList)
+        {
+            List<int> ValidIDs = new List<int>();
+
+            foreach (int ID in IDList)
+            {
+                if (ID.ToString().Length == 9)
+                {
+                    ValidIDs.Add(ID);
+                }
+            }
+
+            return ValidIDs;
+        }
+
+        public List<int> LambdaSutdentIDs(List<int> IDList)
+        {
+            List<int> ValidIDs = IDList.Where(n => n.ToString().Length == 9).ToList();
+
+            return ValidIDs;
+        }
+    }
+}
---
+++ Lambda/TestStudent/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lambda;
+using System.Collections.Generic;
+
+namespace TestStudent
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestStudentID  ()
+        {
+            List<int> TestID = new List<int>() { 123456789, 222222222, 8888, 1010101010 };
+            Student ValidateObject = new Student();
+            List<int> ResultIDs = ValidateObject.ValidSutdentIDs(TestID);
+
+            Assert.IsTrue(ResultIDs.Contains(123456789));
+            Assert.IsTrue(ResultIDs.Contains(222222222));
+            Assert.IsFalse(ResultIDs.Contains(8888));
+            Assert.IsFalse(ResultIDs.Contains(1010101010));
+        }
+
+        [TestMethod]
+        public void TestLambdaStudentID()
+        {
+            List<int> TestID = new List<int>() { 123456789, 222222222, 8888, 1010101010 };
+            Student ValidateObject = new Student();
+            List<int> ResultIDs = ValidateObject.LambdaSutdentIDs(TestID);
+
+            Assert.IsTrue(ResultIDs.Contains(123456789));
+            Assert.IsTrue(ResultIDs.Contains(222222222));
+            Assert.IsFalse(ResultIDs.Contains(8888));
+            Assert.IsFalse(ResultIDs.Contains(1010101010));
+        }
+    }
+}
