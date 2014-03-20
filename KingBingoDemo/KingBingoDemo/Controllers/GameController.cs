3833408619c76ef564c9b9c50b993c63d1bab4c5
bakerr08 <bakerr08@gmail.com>
2014-03-20 10:13:34 -0500

    kingbingo demo

---
+++ KingBingoDemo/KingBingoDemo/Controllers/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KingBingoDemo.Models;
+
+namespace KingBingoDemo.Controllers
+{
+    public class GameController : Controller
+    {
+        kingbingoEntities db = new kingbingoEntities();
+
+        public ActionResult Index(int id)
+        {
+            var game = db.Games.SingleOrDefault(g => g.GameID == id);
+            ViewData["game"] = game;
+
+            return View();
+        }
+
+    }
+}
---
+++ KingBingoDemo/KingBingoDemo/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections;
+using KingBingoDemo.Models;
+using System.Data.Entity;
+
+namespace KingBingoDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        kingbingoEntities db = new kingbingoEntities();
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello World!";
+            var gameID = 1;
+            ViewBag.gameID = gameID;
+
+            DbSet<Game> games = db.Games;
+            ViewData["games"] = games;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
